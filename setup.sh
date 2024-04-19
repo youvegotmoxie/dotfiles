ffb4732bf4ada2dc0331c54344343aee1b08b79a
Michael Beasley <michael.beasley@alvaria.com>
2024-04-19 10:07:18 -0700

    fix dots

--- setup.sh
+++ setup.sh
@@ -4,4 +4,11 @@
 echo "Download Vundle"
 mkdir -pv ${HOME}/.vim/{bundle,undo,colors}
 git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
+
+# Configure vim
+echo "Configuring vim"
 cp -v assets/wombat256.vim "${HOME}/.vim/colors"
+cp -v .vimrc "${HOME}"
+
+# Add remaining dotfiles
+cp -v .better-commits.json .zshrc "${HOME}"
