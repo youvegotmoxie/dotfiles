a1c915364f50395d07abe897ea07d591df309af0
Michael Beasley <youvegotmoxie@gmail.com>
2024-04-19 10:02:13 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 # Setup vim
 echo "Download Vundle"
-mkdir -pv "${HOME}/.vim/{bundle,undo,colors}"
+mkdir -pv ${HOME}/.vim/{bundle,undo,colors}
 git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
 cp -v assets/wombat256.vim "${HOME}/.vim/colors"
